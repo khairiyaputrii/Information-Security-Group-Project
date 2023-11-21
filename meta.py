9010b903d19ce4841cc56ec3a7cd04919f52d0c5
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-11-22 02:24:44 +0700

    encrypting and decryptinh meta data

--- meta.py
+++ meta.py
@@ -2,56 +2,91 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from Cryptodome.Cipher import AES, DES, ARC4
-from Cryptodome.Random import get_random_bytes
+# from Cryptodome.Random import get_random_bytes
 from Cryptodome.Util.Padding import pad, unpad
-from PIL import Image
+# from PIL import Image
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import b64encode, b64decode, standard_b64decode, standard_b64encode
 
-import moviepy.editor as mp
+# import moviepy.editor as mp
 import mysql.connector
 import os
 
 # ! ENCRYPT DECRYPT
 
+def encrypt_message_aes(key, text):
+    text_bytes = text.encode('utf-8')
+    key_bytes = key
+    iv = b'\x00' * 16
+    
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(text_bytes) + padder.finalize()
+    
+    cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    encrypted_text = b64encode(ciphertext).decode('utf-8')
+    
+    return encrypted_text
 
-def encrypt_message_aes(key, plaintext, iv=None):
-    if iv is None:
-        iv = get_random_bytes(16)
+def decrypt_message_aes(key, encrypted_text):
+    encrypted_bytes = b64decode(encrypted_text)
+    key_bytes = key
+    iv = b'\x00' * 16 
+    
+    cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    
+    decrypted_data = decryptor.update(encrypted_bytes) + decryptor.finalize()
+    
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+    
+    decrypted_text = unpadded_data.decode('utf-8')
+    
+    return decrypted_text
+
+def encrypt_message_des(key, plaintext):
     plaintext_bytes = plaintext.encode('utf-8')
-    padded_plaintext = pad(plaintext_bytes, AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = cipher.encrypt(padded_plaintext)
-    return iv + ciphertext
+    key_bytes = key
+    iv = b'\x00' * 8
+    
+    cipher = DES.new(key_bytes, DES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(pad(plaintext_bytes, 8))
+    return b64encode(ciphertext, altchars=None).decode('utf-8')
 
-def decrypt_message_aes(key, ciphertext):
-    iv = ciphertext[:16]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_data = cipher.decrypt(ciphertext[16:])
-    plaintext = unpad(decrypted_data, AES.block_size)
-    return plaintext.decode('utf-8')
-
-def encrypt_message_des(key, plaintext, iv=None):
-    if iv is None:
-        iv = get_random_bytes(8)
-    plaintext_bytes = plaintext.encode('utf-8')
-    padded_plaintext = pad(plaintext_bytes, 8)
-    cipher = DES.new(key, DES.MODE_CBC, iv)
-    ciphertext = cipher.encrypt(padded_plaintext)
-    return iv + ciphertext
-
-def decrypt_message_des(key, ciphertext):
-    iv = ciphertext[:8]
-    cipher = DES.new(key, DES.MODE_CBC, iv)
-    decrypted_data = cipher.decrypt(ciphertext[8:])
-    plaintext = unpad(decrypted_data, 8)
-    return plaintext.decode('utf-8')
+def decrypt_message_des(key, encrypted_text):
+    encrypted_bytes = b64decode(encrypted_text, altchars=None, validate=True)
+    key_bytes = key
+    iv = b'\x00' * 8
+    
+    cipher = DES.new(key_bytes, DES.MODE_CBC, iv)
+    decrypted_data = cipher.decrypt(encrypted_bytes)
+    
+    plaintext = unpad(decrypted_data, 8).decode('utf-8')
+    
+    return plaintext
 
 def encrypt_message_arc4(key, plaintext):
-    cipher = ARC4.new(key)
-    plaintext = plaintext.encode('utf-8')
-    ciphertext = cipher.encrypt(plaintext)
-    return ciphertext
+    plaintext_bytes = plaintext.encode('utf-8')
+    key_bytes = key
+    
+    cipher = ARC4.new(key_bytes)
+    ciphertext = cipher.encrypt(plaintext_bytes)
+    
+    return b64encode(ciphertext, altchars=None).decode('utf-8')
 
-def decrypt_message_arc4(key, ciphertext):
-    cipher = ARC4.new(key)
-    plaintext = cipher.decrypt(ciphertext)
+def decrypt_message_arc4(key, encrypted_text):
+    encrypted_bytes = b64decode(encrypted_text, altchars=None, validate=True)
+    key_bytes = key
+    
+    cipher = ARC4.new(key_bytes)
+    decrypted_data = cipher.decrypt(encrypted_bytes)
+    
+    plaintext = decrypted_data.decode('utf-8')
+    
     return plaintext
+
