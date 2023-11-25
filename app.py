6761a7eff28fe453d35d2109abe730bdc2b2e96f
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-11-25 13:41:40 +0700

    save

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from backend.routes.main import root as routes_root
 from backend.routes.authentication import authentication as routes_authentication
 from backend.routes.encryption import encryption as routes_encryption
 from backend.routes.decryption import decryption as routes_decryption
-from backend.routes.request import request as routes_request
+from backend.routes.request import request_blueprint
 
 app = Flask(__name__)
 app.secret_key = "secret_key_for_flash_messages"
@@ -20,7 +20,8 @@ app.register_blueprint(routes_root)
 app.register_blueprint(routes_authentication)
 app.register_blueprint(routes_encryption)
 app.register_blueprint(routes_decryption)
-app.register_blueprint(routes_request)
+# app.register_blueprint(routes_request)
+app.register_blueprint(request_blueprint, url_prefix='/request')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/routes/request.py
+++ backend/routes/request.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, Flask, render_template, request, redirect, url_for, flash, session
 from backend.database.db import create_connection
 
-request = Blueprint("request", __name__)
+request_blueprint = Blueprint("request_blueprint", __name__)
 
-@request.route("/request")
+@request_blueprint.route("/request", methods=["GET"])
 def req():
     if "username" not in session:
         flash("You must log in first to access this page.", "flash-warning")
@@ -31,7 +31,7 @@ def req():
     
     return render_template("request.html", result=result, result_two=result_two)
 
-@request.route("/request_to", methods=["GET","POST"])
+@request_blueprint.route("/request_to", methods=["POST"])
 def req_to():
     if "username" not in session:
         flash("You must log in first to access this page.", "flash-warning")
@@ -39,20 +39,19 @@ def req_to():
 
     username = session["username"]
     userID = session["userID"]
-    
-    if request.method == "POST":
-        user_destination = request.form["user_destination"]
 
-        connection = create_connection()
-        cursor = connection.cursor()
-        insert_data_query = """
-        INSERT INTO request (
-            source_id,
-            destination_id
-        ) VALUES (%s, %s)
-        """
-        data = (username, user_destination)
-        cursor.execute(insert_data_query, data)
+    user_destination = request.form["user_destination"]
+
+    connection = create_connection()
+    cursor = connection.cursor()
+    insert_data_query = """
+    INSERT INTO request (
+        source_id,
+        destination_id
+    ) VALUES (%s, %s)
+    """
+    data = (username, user_destination)
+    cursor.execute(insert_data_query, data)
 
     connection.commit()
     cursor.close()
