0d0e90199b573904b71c99858da8d1f16e307996
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-11-25 12:26:00 +0700

    update

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ import os
 import io
 
 from backend.database.db import create_connection
+from backend.routes.main import root as routes_root
 from backend.routes.authentication import authentication as routes_authentication
 from backend.routes.encryption import encryption as routes_encryption
 from backend.routes.decryption import decryption as routes_decryption
+from backend.routes.request import request as routes_request
 
 app = Flask(__name__)
 app.secret_key = "secret_key_for_flash_messages"
 
+app.register_blueprint(routes_root)
 app.register_blueprint(routes_authentication)
 app.register_blueprint(routes_encryption)
 app.register_blueprint(routes_decryption)
+app.register_blueprint(routes_request)
 
 if __name__ == "__main__":
     app.run(debug=True)
